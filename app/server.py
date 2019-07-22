1efe5cf0c76d540c7c6406948b831e51d6b82071
panjinjin <jinjin.pan@hotmail.com>
2019-07-22 15:46:25 -0400

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/sh/dn8sokf0iy1qjih/AAAJYlhof3mG_fKI8wl6bFSva?dl=0'
+export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
 export_file_name = 'export.pkl'
 
 classes = ['groundhog', 'otter', 'beaver']
